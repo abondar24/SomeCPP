d0c2ed679ba122bf79374d96264bb54d0da51d39
Dr762 <abondar1992@gmail.com>
2016-05-17 01:03:40 +0300

    lambdas, algorithms

--- base/base3.cpp
+++ base/base3.cpp
@@ -4,10 +4,15 @@
 
 #include <iostream>
 #include <vector>
-#include <limits>
 #include <unordered_map>
+#include <functional>
+#include <algorithm>
+
 
 using namespace std;
+function<int(void)> multBy2(int x);
+function<int(void)> multBy3(int x);
+void testCallBack(const vector<int>& vec, const function<bool(int)>& callback);
 
 auto main() -> int {
     vector<int> vector1(10, 0);
@@ -47,6 +52,72 @@ auto main() -> int {
         cout << p.first << " = " << p.second << endl;
     }
 
+    []{cout <<"Test lambda"<<endl;}();
+
+    string result =[](const string& str){ return "Hello from "+str;}("California");
+    cout<<"Result: "<<result<<endl;
+
+    auto fn = [](const string& str){ return "Hello from "+str;};
+
+    cout<<fn("Massachusets")<<endl;
+
+    auto fn2 = multBy2(5);
+    cout<<fn2()<<endl;
+
+    auto fn3 = multBy3(7);
+    cout<<fn3()<<endl;
+
+    int k=8;
+    testCallBack(vector1,[=,&k](int i){return i<k;});
+
+    vector<int> vector4 = {1,2,3,4,5,6,7,8,9,10,11};
+    int index =0;
+    for_each(vector4.begin(),vector4.end(),[&index](int i){cout<<i*4<<":"<<(index++)<<endl;});
+
+
+    //function object adapter
+    //find first greater than 2
+    //,placeholders::_1 - implementation defined maximum number.
+    // When used as an argument in a std::bind expression, the placeholder objects are stored
+    // in the generated function object, and when that function object is invoked with unbound arguments,
+    // each placeholder _N is replaced by the corresponding Nth unbound argument.
+    auto it = find_if(vector4.begin(),vector4.end(),bind(greater<int>(),placeholders::_1,2));
+    cout << *it<<endl;
+
+    //we also have mx,search,first_of,find_n - see the doc
+    auto it1 = min_element(vector4.begin(),vector4.end());
+    cout << *it1<<endl;
+
+    //more readable than greater example
+    auto  it2 = find_if(vector4.begin(),vector4.end(),[](int i){ return i%2==0;});
+    cout << *it2<<endl;
+
+    transform(vector4.begin(),vector4.end(),vector4.begin(),[](int i){ return i*200;});
+    for (auto& i: vector4){
+        cout << i<<endl;
+
+    }
+
 
     return 0;
+}
+
+//function wrapper
+function<int(void)> multBy2(int x){
+    //capture by val
+    return [=]()->int{ return 2*x;};
+}
+
+function<int(void)> multBy3(int x){
+    //capture by ref
+    return [&]()->int{ return 3*x;};
+}
+
+void testCallBack(const vector<int>& vec, const function<bool(int)>& callback){
+    for (auto i:vec){
+        if (!callback(i))
+            break;
+        cout<<i<<" ";
+    }
+    cout << endl;
 }
